c19ce48ece3a6ed7246ac6906c7f5227b925d448
Ricardo Rojas <ricardorojasm1991@gmail.com>
2025-01-16 20:12:03 -0600

    update data_ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+# Data ingestion configuration class to store the data paths
+@dataclass
+class DataIngestionConfig:
+    data_path: str = os.path.join("artifacts", "data.csv")
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+
+
+# Data ingestion class to load the data and split it into train and test data
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def load_data(self):
+        logging.info("Initiating data ingestion")
+        try:
+            # Loading the data from the data path
+            data = pd.read_csv("src/notebook/data/students.csv")
+            logging.info("Data loaded successfully")
+
+            # Creating folder for train and test data if it does not exist
+            os.makedirs(os.path.dirname(self.config.data_path), exist_ok=True)
+
+            # Saving the data to the data path
+            data.to_csv(self.config.data_path, index=False, header=True)
+
+            # Splitting the data into train and test data
+            logging.info("Splitting data into train and test data")
+            train_data, test_data = train_test_split(
+                data, test_size=0.2, random_state=42
+            )
+
+            # Saving the train and test data to the respective paths
+            train_data.to_csv(self.config.train_data_path, index=False, header=True)
+            test_data.to_csv(self.config.test_data_path, index=False, header=True)
+
+            logging.info("Data ingestion completed successfully")
+
+            return (
+                self.config.train_data_path,
+                self.config.test_data_path,
+            )
+        except Exception as e:
+            logging.error("Error loading data")
+            raise CustomException(e, sys)
